86172b7d7862f8e750fa7b156c68edd2cbc388b3
AnujNdas <anujd339@gmail.com>
2024-04-07 08:19:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+ScrollReveal({
+    reset:false,
+    distance:'100px',
+    duration: 500,
+    delay:50
+});
+ScrollReveal().reveal('.container',{ duration:1500});
+ScrollReveal().reveal('.it',{ duration:1500});
+ScrollReveal().reveal('.flex',{ duration:1500});
+ScrollReveal().reveal('.tic',{ duration:1500});
+ScrollReveal().reveal('.asus',{ duration:1500,origin:'bottom'});
+ScrollReveal().reveal('.box6',{ durduration:1500,origin:'left'});
+ScrollReveal().reveal('.box7',{ durduration:1500,origin:'right'});
+ScrollReveal().reveal('.imr',{ duration:1500});
+ScrollReveal().reveal('.eng',{ duration:1500, origin:'top'});
+ScrollReveal().reveal('.pic2',{ duration:1500,origin:'right'});
+ScrollReveal().reveal('.pic1',{ duration:1500,origin:'left'});
+ScrollReveal().reveal('.box1',{ duration:1500,origin:'bottom'});
+ScrollReveal().reveal('.box2',{ duration:1500,origin:'bottom'});
+ScrollReveal().reveal('.box3',{ duration:1500,origin:'top'});
+ScrollReveal().reveal('.box4',{ duration:1500,origin:'bottom'});
+ScrollReveal().reveal('.box5',{ duration:1500,origin:'right'});
+ScrollReveal().reveal('.box8',{ duration:1500,origin:'left'});
+ScrollReveal().reveal('.box9',{ duration:1500,origin:'left', delay:200});
+ScrollReveal().reveal('.box10',{ duration:1500,origin:'left',  delay:300});
